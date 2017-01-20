630340fcb0fdfe6144a1b921a6a6ee2f230941ff
pengchao <chaoyue2010@yeah.net>
2017-01-20 22:10:26 +0800

    copy积分部分源码

---
+++ extremeworld/src/main/java/com/jixianxueyuan/service/PointService.java
@@ -0,0 +1,124 @@
+package com.jixianxueyuan.service;
+
+import java.util.Date;
+import java.util.List;
+
+import com.jixianxueyuan.config.PointType;
+import com.jixianxueyuan.entity.Point;
+import com.jixianxueyuan.entity.User;
+import com.jixianxueyuan.push.PushManage;
+import com.jixianxueyuan.push.PushMessageType;
+import com.jixianxueyuan.repository.PointDao;
+import com.jixianxueyuan.repository.UserDao;
+import com.jixianxueyuan.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+//Spring Bean的标识.
+@Component
+//类中所有public函数都纳入事务管理的标识.
+@Transactional
+public class PointService {
+
+	@Autowired
+	private PointDao pointDao;
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	private PushManage pushManage;
+	
+	public Point getPoint(long id){
+		return pointDao.findOne(id);
+	}
+	
+	public long getUserTotalPoint(long userId){
+		User user = userDao.findById(userId);
+		if (user != null) {
+			return user.getPoint();
+		}
+		return 0;
+	}
+	
+	//返回增加积分,0为没有增加，大于0为增加
+	//规则：每日登陆赠送一次积分，每日前3条主题赠送积分，每日前5条回复赠送积分
+	public int addPoint (PointType pointType, Long userId) {
+		Point point = new Point();
+		User user = userDao.findById(userId);
+		if (user != null) {
+			point.setUser(user);
+			Date todayDate = DateUtils.getTodayBeginDate();
+			if (pointType.equals(PointType.LOGIN)) {
+				List<Point> todayLoginPointList = pointDao.findByTypeAndUserIdAndTime(userId,PointType.LOGIN.getType(),  todayDate);
+				if (todayLoginPointList == null || todayLoginPointList.size() < 1) {
+					point.setType(PointType.LOGIN.getType());
+					point.setCount(PointType.LOGIN.getCount());
+					point.setDes("每日登录  +" + String.valueOf(PointType.LOGIN.getCount()) + "积分");
+				}else {
+					return 0;
+				}
+
+			}else if(pointType.equals(PointType.TOPIC)){
+				List<Point> todayTopicPointList = pointDao.findByTypeAndUserIdAndTime(userId,PointType.TOPIC.getType(),  todayDate);
+				if (todayTopicPointList == null || todayTopicPointList.size() < 3) {
+					point.setType(PointType.TOPIC.getType());
+					point.setCount(PointType.TOPIC.getCount());
+					point.setDes("发布主题 +" + String.valueOf(PointType.TOPIC.getCount()) + "积分");
+				}else {
+					return 0;
+				}
+
+			}else if(pointType.equals(PointType.REPLY)){
+				List<Point> todayReplyPointList = pointDao.findByTypeAndUserIdAndTime(userId,PointType.REPLY.getType(),  todayDate);
+				if (todayReplyPointList == null || todayReplyPointList.size() < 5) {
+					point.setType(PointType.REPLY.getType());
+					point.setCount(PointType.REPLY.getCount());
+					point.setDes("回复主题 +" + String.valueOf(PointType.REPLY.getCount())  + "积分");
+				}else {
+					return 0;
+				}
+
+			}else {
+				return 0;
+			}
+		
+			save(point);
+			user.setPoint(user.getPoint() + point.getCount());
+			userDao.save(user);
+			
+			pushManage.pushMessage(user, PushMessageType.POINT, point);
+			return point.getCount();
+		}
+		
+		return 0;
+	}
+	
+	private Point save(Point point){
+		return pointDao.save(point);
+	}
+	
+	public Page<Point> getAll(Long userId, int pageNumber, int pageSize,String sortType) {
+		PageRequest pageRequest = buildPageRequest(pageNumber, pageSize, sortType);
+		return pointDao.findByUserId(userId, pageRequest);
+	}
+	
+	/**
+	 * 创建分页请求.
+	 */
+	private PageRequest buildPageRequest(int pageNumber, int pagzSize, String sortType) {
+		Sort sort = null;
+		if ("auto".equals(sortType)) {
+			sort = new Sort(Direction.DESC, "id");
+		} else {
+			sort = new Sort(Direction.DESC, "id");
+		}
+		return new PageRequest(pageNumber - 1, pagzSize, sort);
+	}
+}
