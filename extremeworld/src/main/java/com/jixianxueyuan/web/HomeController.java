0d8d055455b27c931c17d15cfd21363e79a2d492
unknown <>
2017-02-11 16:09:47 +0800

    配置thymeleaf  添加semantic-ui  添加home页

---
+++ extremeworld/src/main/java/com/jixianxueyuan/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.jixianxueyuan.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * Created by 23653 on 2016/12/20.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+}
